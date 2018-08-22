0a7298b7aa8994e6567f327f980bbb3bfcce5d2c
Jose Lopez <jalopez1423@gmail.com>
2018-08-22 16:07:16 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### :point_right: This starter repo has moved to the [ionic-team/starters](https://github.com/ionic-team/starters/tree/master/ionic-angular/official/blank) repo! :point_left:
+
+#### Left off here
+https://youtu.be/mNVOk3Dpcr4?list=PLYxzS__5yYQng-XnJhB21Jc7NW1OIaqct
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { FirstPage } from '../pages/first/first';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    FirstPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    FirstPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
